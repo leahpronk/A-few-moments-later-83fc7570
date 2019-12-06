00eeb17a9d5034a6d1c8b1557bcc7727d6f15f65
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2019-12-06 11:48:03 +0100

    Uitwerking van de exercise

--- tijd.php
+++ tijd.php
@@ -1,8 +1,27 @@
 <?php
 
-$s = readline();
-$letter=strpos($s, "s");
-if ($letter){
-    print str_replace("s", "seconden" , $s);
-} else{
-    print "geen tijd";
+<?php
+$b= explode(" " , $argv[1]);
+$c= 0;
+foreach ($b as $value) 
+{
+$a = substr($value, -1);
+switch ($a) {
+case 'd':
+ $c=$c+((int)$value * 86400);
+break;
+case 'h':
+ $c=$c+((int) $value * 3600);
+break;
+case 'm':
+ $c=$c+((int) $value * 60);
+break;
+case 's':
+ $c=$c+(int) $value;
+break;
+default:
+ echo "is geen tijd";
+break;
+}
+}
+echo "${c} seconde";
